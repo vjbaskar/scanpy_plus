9d18787f4a04d1a8bdd73dea641b6de2fe3a3ac1
Vijaya Baskar <34027254+vjbaskar@users.noreply.github.com>
2025-05-15 09:46:02 +0100

    Update liana_cc_rankaggr.py

--- scanpy_minion/tl/liana_cc_rankaggr.py
+++ scanpy_minion/tl/liana_cc_rankaggr.py
@@ -1,17 +1,24 @@
-def liana_cc_rankaggr(adata):
+from anndata import AnnData
+from typing import Union
 
-import liana as li
 
+def liana_cc_rankaggr(adata: AnnData,
+    use_raw: bool = False,
+    groupby: str,
+    resource_name: str):
+    """
+    
     """
     This function calculates consensus ligand-receptor predictions  of individual methods. This is done by ranking and aggregating (RRA) the ligand-receptor interaction predictions from all methods.
     adata: anndata obj
     
     return: anndata with liana ligand-receptor interactions consensus results
     """
-	li.mt.rank_aggregate(adata_filtered_dr,use_raw=False,
-                     groupby='celltype',
-                     resource_name='consensus',
-                     expr_prop=0.1
-                     verbose=True, **kwargs)
-	rank_aggregate.describe()
+    import liana as li
+    li.mt.rank_aggregate(adata_filtered_dr,use_raw=False,
+		     groupby=groupby,
+		     resource_name='consensus',
+		     expr_prop=0.1
+		     verbose=True, **kwargs)
+    rank_aggregate.describe()
 
