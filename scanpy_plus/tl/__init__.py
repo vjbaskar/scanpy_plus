cfafff4f59a9dd0928cc4439922883263f728397
Vijaya Baskar <34027254+vjbaskar@users.noreply.github.com>
2025-07-08 03:45:59 +0100

    Update __init__.py
    Removed unused imports

--- scanpy_plus/tl/__init__.py
+++ scanpy_plus/tl/__init__.py
@@ -2,6 +2,6 @@
 # from .globimport import *
 from .rankobs import * # Rank obs as you want. Useful for filtering
 from .gsea import * # GSEA using gseapy
-from .map_to_dahlin import * # Map to dahlin
+#from .map_to_dahlin import * # Map to dahlin
 from .tryumap import tryumap # umap from scaling
 from .run_scvi import run_scvi
